be66cfd56afa7c0167f5cc0f2ba77c0bd07c5545
lincoln <huylincoln007@yahoo.com>
2017-07-07 14:01:36 -0700

    Add unique ID to each team

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
   private String mName;
+  private static List<Team> instances = new ArrayList<Team>();
 
 
   public Team(String name) {
     mName = name;
+    instances.add(this);
   }
 
   public String getName() {
     return mName;
+  }
+
+  public static List<Team> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instance.clear();
   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -13,4 +13,19 @@ public class TeamTest {
     Team myTeam = new Team("Barbell");
     assertEquals("Barbell", myTeam.getName());
   }
+
+  @Test
+  public void all_returnsAllTeam_true() {
+    Team firstTeam = new Team("Barbell");
+    Team secondTeam = new Team("Brigade");
+    assertEquals(true, Team.all().contains(firstTeam));
+    assertEquals(true, Team.all().contains(secondTeam));
+  }
+
+  @Test
+  public void getId_teamsWithId_1() {
+    Team.clear();
+    Team myTeam = new Team("Barbell");
+    assertEquals(1, myTeam.getId());
+  }
 }
