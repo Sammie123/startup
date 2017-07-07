043d4200e2edc9fd8ee1671044b74a4967971cd2
lincoln <huylincoln007@yahoo.com>
2017-07-07 13:03:19 -0700

    Test application and fix errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -11,7 +12,26 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("teams", request.session().attribute("teams"));
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Team> teams = request.session().attribute("teams");
+      if (teams == null) {
+        teams = new ArrayList<Team>();
+        request.session().attribute("teams", teams);
+      }
+
+      String name = request.queryParams("name");
+      Team newTeam = new Team(name);
+      teams.add(newTeam);
+      request.session().attribute("team", newTeam);
+
+      model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
