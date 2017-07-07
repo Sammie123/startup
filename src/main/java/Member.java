8d4dd5922a61dc72e470a3f2772b5eb7e491ee67
lincoln <huylincoln007@yahoo.com>
2017-07-07 15:22:38 -0700

    connecting member and team methods

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,11 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Member {
   private String mName;
+  private static List<Member> instances = new ArrayList<Member>();
+  private int mId;
+  private List<Team> mTeams;
 
   public Member(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
+    mTeams = new ArrayList<Team>();
   }
 
   public String getName() {
     return mName;
+  }
+
+  public static List<Member> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Member find(int id) {
+    return instances.get(id - 1);
+  }
+
+  public List<Team> getTeams() {
+    return mTeams;
+  }
+
+  public void addTeam(Team team) {
+    mTeams.add(team);
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -14,4 +14,33 @@ public class MemberTest {
     Member testMember = new Member("Lincoln");
     assertEquals("Lincoln", testMember.getName());
   }
+
+  @Test
+  public void all_returnMember_true() {
+    Member firstMember = new Member("Lincoln");
+    Member secondMember = new Member("Sam");
+    assertEquals(true, Member.all().contains(firstMember));
+    assertEquals(true, Member.all().contains(secondMember));
+  }
+
+  @Test
+  public void clear_clearMembers_0() {
+    Member testMember = new Member("Lincoln");
+    Member.clear();
+    assertEquals(Member.all().size(), 0);
+  }
+
+  @Test
+  public void getId_memberId_1() {
+    Member testMember = new Member("Lincoln");
+    assertEquals(1, testMember.getId());
+  }
+
+  @Test
+  public void addTeam_addsTeamToMembers_true(){
+    Member testMember = new Member("Lincoln");
+    Team testTeam = new Team("Barbell");
+    testMember.addTeam(testTeam);
+    assertTrue(testMember.getTeams().contains(testTeam));
+  }
 }
